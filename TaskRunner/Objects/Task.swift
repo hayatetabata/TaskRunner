d1060da25c93ce0e76ec7bccb16f79db9b309301
田畑颯 <chinarra.tabata@gmail.com>
2018-07-28 19:18:07 +0900

    Add task object

---
+++ TaskRunner/Objects/Task.swift
@@ -0,0 +1,15 @@
+//
+//  Todo.swift
+//  TaskRunner
+//
+//  Created by 田畑颯 on 2018/07/28.
+//  Copyright © 2018年 田畑颯. All rights reserved.
+//
+
+import Foundation
+
+struct Task {
+    let id: Int! = nil
+    var name = ""
+    var deadline: NSDate! = nil
+}
