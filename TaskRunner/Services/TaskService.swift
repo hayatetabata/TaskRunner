616b314a9eae86f64907d5ae59b43e17be24420c
田畑颯 <chinarra.tabata@gmail.com>
2018-07-28 22:25:14 +0900

    Create task service

---
+++ TaskRunner/Services/TaskService.swift
@@ -0,0 +1,37 @@
+//
+//  TodoService.swift
+//  TaskRunner
+//
+//  Created by 田畑颯 on 2018/07/28.
+//  Copyright © 2018年 田畑颯. All rights reserved.
+//
+
+import Foundation
+
+class TaskService {
+    func search () {
+        
+    }
+    
+    func findOrFail (taskId: Int) {
+        
+    }
+    
+    func create (attributes: Dictionary) {
+        let task: Task = Task()
+        return task
+    }
+    
+    func update (task: Task, attributes: Dictionary) {
+        let task: Task = Task()
+        return task;
+    }
+    
+    func delete (task: Task) {
+        
+    }
+    
+    func finish (task: Task) {
+        
+    }
+}
