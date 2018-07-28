3a1ebe826a9b14aaf5ddb80b79f90911fe6373aa
田畑颯 <chinarra.tabata@gmail.com>
2018-07-28 14:13:30 +0900

    Initial Commit

---
+++ TaskRunner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaskRunner
+//
+//  Created by 田畑颯 on 2018/07/28.
+//  Copyright © 2018年 田畑颯. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
