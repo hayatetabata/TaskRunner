ff7c5d9883efcd51dac3f6d4386794fff74bfb46
田畑颯 <chinarra.tabata@gmail.com>
2018-07-28 15:41:56 +0900

    Add table cell view

--- TaskRunner/ViewController.swift
+++ TaskRunner/ViewController.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var TableView: UITableView!
+    
+    let fruits = ["apple", "orange", "melon", "banana", "pineapple"];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fruits.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "todo", for: indexPath)
+        
+        cell.textLabel!.text = fruits[indexPath.row]
+        
+        return cell;
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
