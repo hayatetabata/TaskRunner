c66c01359b7d0952109de20d96aff1681b452c40
田畑颯 <chinarra.tabata@gmail.com>
2018-07-28 18:55:31 +0900

    Add todo editor controller

---
+++ TaskRunner/TodoEditorController.swift
@@ -0,0 +1,35 @@
+//
+//  TodoEditorController.swift
+//  TaskRunner
+//
+//  Created by 田畑颯 on 2018/07/28.
+//  Copyright © 2018年 田畑颯. All rights reserved.
+//
+
+import UIKit
+
+class TodoEditorController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
